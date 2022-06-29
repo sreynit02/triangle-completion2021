1f4b90b7a926541d69ecb2d3a84b01a181f82397
Sreynit Khatt <khatts@berea.edu>
2022-06-29 12:31:23 -0500

    initial update

---
+++ Assets/Assets/Scripts/OutCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutCollider : MonoBehaviour {
+
+	public GameObject outBoundPost;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("ColliderSp"))
+		{
+			outBoundPost.SetActive(false);
+		}
+	}
+}
